f8f04a698742cc4b19a2dc9ae994cf39617468a4
Syahid Reza Mutahari <syahidreza@gmail.com>
2020-06-28 10:04:40 +0700

    fix admin login logo

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="<?= base_url(); ?>">Sanggar Seni <b>Asem Gede</b></a>
+    <a href="<?= base_url(); ?>">Sanggar Seni Asem Gede</a>
   </div>
 
   <!-- notif -->
